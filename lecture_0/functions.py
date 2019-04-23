f48b5dd798cd03a81ea810b33af10e42ab7ae24d
dmitry.vilesov <vilesovds@mail.ru>
2019-04-23 15:20:13 +0300

    #32770: Fixed names for variables

--- lecture_0/functions.py
+++ lecture_0/functions.py
@@ -1,6 +1,6 @@
 def is_name(name):
     try:
-        if name.istitle():  # Первая буква заглавная, остальные неважно
+        if name.istitle():
             result = True
         else:
             result = False
@@ -29,23 +29,21 @@ def increase_list(var1, var2, var3):
     return [var1 * 2, var2 * 2]
 
 
-def filter_list(some_list):
-    for i in some_list.copy():
+def filter_list(list_to_filter):
+    for i in list_to_filter.copy():
         try:
             if i <= 0:
-                some_list.remove(i)
+                list_to_filter.remove(i)
         except TypeError:
             print(f'Элемент {i} не числовой и не может быть обработан')
 
-    return some_list
 
-
-def max_list(some_list, var1, var2):
+def max_list(list_to_extend, var1, var2):
     try:
         if var1 > var2:
-            some_list.append(var1)
+            list_to_extend.append(var1)
         elif var1 < var2:
-            some_list.append(var2)
+            list_to_extend.append(var2)
     except TypeError:
         print('2 и 3 аргументы несравнимы между собой')
 
@@ -54,13 +52,13 @@ def get_dictionary(key, value):
     return {key: value}
 
 
-def set_dictionary(some_dict, key, value):
-    return some_dict.update({key: value})
+def set_dictionary(dict_to_extend, key, value):
+    return dict_to_extend.update({key: value})
 
 
-def swap_dictionary(some_dict):
-    return {value: key for key, value in some_dict.items()}
+def swap_dictionary(dict_to_swap):
+    return {value: key for key, value in dict_to_swap.items()}
 
 
-def increase(some_int):
-    return lambda another_int: another_int * some_int
+def increase(multiplier):
+    return lambda var_to_multiply: var_to_multiply * multiplier
