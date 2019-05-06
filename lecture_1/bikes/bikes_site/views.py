4c761d976fc0f97c0574fbcffd61d99e6addbb0d
dmitry.vilesov <vilesovds@mail.ru>
2019-05-06 03:35:42 +0300

    #33202: flake8 fixes

--- lecture_1/bikes/bikes_site/views.py
+++ lecture_1/bikes/bikes_site/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, JsonResponse
 from django.views.generic import ListView, DetailView
 from .models import Category, Motobike
 # import json
-from django.shortcuts import get_object_or_404
+# from django.shortcuts import get_object_or_404
 
 
 # Создайте представления, исходя из urls
